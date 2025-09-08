45081e35b82a76ad2531f901f6fd2a1f91c49444
peeljunKim <kimkr7410@naver.com>
2025-09-08 21:58:09 +0900

    react-router 간단한 라우터 설정

---
+++ front/src/pages/MainPage.tsx
@@ -0,0 +1,16 @@
+// import { NavLink } from 'react-router'
+/**
+ * NavLink를 사용하면 MainPage, AboutPage를 가져옵니다 불 필요한 리소스 가져옴
+ */
+function MainPage() {
+  return (
+    <div className="text-3xl">
+      <div>mainPage</div>
+      {/* <div className="flex">
+        <NavLink to="/about">About</NavLink>
+      </div> */}
+    </div>
+  )
+}
+
+export default MainPage
---
+++ front/src/router/router.tsx
@@ -0,0 +1,64 @@
+import { createBrowserRouter } from 'react-router'
+import { lazy, Suspense } from 'react'
+import BasicLayout from '../layouts/BasicLayout'
+
+/**
+ * lazy: 레이징 로딩 (필요할 때 까지 로딩을 미루는 기능)
+ * Suspense: fallback 옵션은 기다리는 동작 보여지는 페이지
+ * 본문에는 React.lazy()를 이용해서 동적 임포트 작성 import() 함수를 사용하면 필요한 시점에 모듈을 로드할 수 있습니다.
+ * 아래와 같이 작성하면 각 라우트에 접속할 때 필요한 컴포넌트만 따로 불러와 초기 로딩 시간을 단축하는 분할 로딩이 구현
+ */
+const Loading = () => <div>Loading...</div>
+const Main = lazy(() => import('../pages/MainPage'))
+const About = lazy(() => import('../pages/AboutPage'))
+
+/**
+ * React Router v6 이후에 라우트 구성 방식이 변경 - 두 번째 코드: React Router v6.4+의 최신 방식
+ *
+ */
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: (
+//       <Suspense fallback={<Loading />}>
+//         <Main />
+//       </Suspense>
+//     ),
+//   },
+//   {
+//     path: '/aboutPage',
+//     element: (
+//       <Suspense fallback={<Loading />}>
+//         <About />
+//       </Suspense>
+//     ),
+//   },
+// ])
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    Component: BasicLayout,
+    children: [
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Main />
+          </Suspense>
+        ),
+      },
+      {
+        path: 'about',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <About />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+])
+
+export default router
