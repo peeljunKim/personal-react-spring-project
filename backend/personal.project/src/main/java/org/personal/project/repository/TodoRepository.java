5e7c10cecfd11f14c070cf99e95c6a0551e6e7d1
peeljunKim <kimkr7410@naver.com>
2025-09-10 23:45:55 +0900

    todo 엔티티 추가 및 테스트

---
+++ backend/personal.project/src/main/java/org/personal/project/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package org.personal.project.repository;
+
+import org.personal.project.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
