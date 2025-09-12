7495ff3d1e070ff905ed05dc1731bf2fa1808fd6
peeljunKim <kimkr7410@naver.com>
2025-09-13 02:41:39 +0900

    Todo 조회 기능 구현

--- front/src/pages/todo/ReadPage.tsx
+++ front/src/pages/todo/ReadPage.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router'
+import ReadComponent from '../../components/todo/ReadComponent'
 
 /**
  * useParams: 동적 매개변수의 값을 가져옴 (문자열(객체)로 가져옴)
@@ -11,6 +12,7 @@ function ReadPage() {
   return (
     <div className="bg-white w-full">
       <div className="text-4xl">Todo Read Page {tno}</div>
+      <ReadComponent tno={Number(tno)} />
     </div>
   )
 }
---
+++ front/src/types/global.d.ts
@@ -0,0 +1,5 @@
+// .d는 선언 파일을 의미하며, 타입 정의만 담고 있는 파일
+interface PageParam {
+  page?: string | number
+  size?: string | number
+}
