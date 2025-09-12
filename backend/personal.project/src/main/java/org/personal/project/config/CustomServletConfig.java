807d9ca88e0342a24013bae66b7a5055d183f22e
peeljunKim <kimkr7410@naver.com>
2025-09-12 17:39:22 +0900

    Add RestControllerAdvice, LocalDateFormatter

---
+++ backend/personal.project/src/main/java/org/personal/project/config/CustomServletConfig.java
@@ -0,0 +1,31 @@
+package org.personal.project.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.personal.project.controller.formatter.LocalDateFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Log4j2
+public class CustomServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+        log.info("addFormatters - LocalDateFormatter 시작");
+        registry.addFormatter(new LocalDateFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+    }
+
+}
