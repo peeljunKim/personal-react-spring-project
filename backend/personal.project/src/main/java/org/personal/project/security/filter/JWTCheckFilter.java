b76acbf707d675ffc0df71538e888d51ad8b5121
peeljunKim <kimkr7410@naver.com>
2025-09-21 22:23:33 +0900

    [Add] UsernamePasswordAuthenticationToken 동작 원리 및 순서

--- backend/personal.project/src/main/java/org/personal/project/security/filter/JWTCheckFilter.java
+++ backend/personal.project/src/main/java/org/personal/project/security/filter/JWTCheckFilter.java
@@ -79,6 +79,25 @@ public class JWTCheckFilter extends OncePerRequestFilter {
             UsernamePasswordAuthenticationToken authenticationToken =
                     new UsernamePasswordAuthenticationToken(memberDTO, pw, memberDTO.getAuthorities());
 
+            /**
+             * SecurityContextHolder를 사용하는 방식은 무상태인 것처럼 보이지만, 매 요청마다 상태를 재생성한다는 단점을 가지고 있습니다
+             *
+             * SecurityContextHolder는 서버의 메모리(스레드 로컬)에 현재 요청을 보낸
+             * 사용자의 인증 정보(authenticationToken)를 저장하는 역할을 합니다.
+             * 즉, 요청이 처리되는 동안만 사용자의 상태를 임시로 저장한 뒤 사용한 후 바로 삭제
+             *
+             * 이 과정이 없으면 @PreAuthorize와 같은 스프링 시큐리티의 고급 기능들을 제대로 활용할 수 없게 됩니다
+             *
+             * 1. 클라이언트가 보낸 JWT 토큰을 검증합니다.
+             *
+             * 2. 토큰이 유효하면, 토큰의 정보를 기반으로 사용자의 인증 객체(authenticationToken)를 만듭니다.
+             *
+             * 3. 이 인증 객체를 SecurityContextHolder에 저장합니다.
+             *
+             * 4.컨트롤러나 서비스 계층의 @PreAuthorize 같은 어노테이션이 SecurityContextHolder에 저장된 정보를 사용하여 권한을 확인합니다.
+             *
+             * 5. 요청 처리가 끝나면 SecurityContextHolder의 정보는 초기화됩니다.
+             */
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
             filterChain.doFilter(request, response);
