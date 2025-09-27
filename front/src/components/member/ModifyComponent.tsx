e80eaf3765a15f32fa474b78c9bb5d3c88b92db2
peeljunKim <kimkr7410@naver.com>
2025-09-27 20:31:22 +0900

    [fix] Implements member modify

--- front/src/components/member/ModifyComponent.tsx
+++ front/src/components/member/ModifyComponent.tsx
@@ -1,11 +1,69 @@
+import { useActionState } from 'react'
 import useCustomLogin from '../../hooks/useCustomLogin'
+import jwtAxios from '../../util/JwtUtil'
+import ResultModal from '../common/ResultModal'
+import PendingModal from '../common/PendingModal'
+
+interface ModifyResult {
+  result: string
+  error: string
+}
+
+const initState: ModifyResult = {
+  result: '',
+  error: '',
+}
+
+const modifyAction = async (state: ModifyResult, formData: FormData) => {
+  const email = formData.get('email') as string
+  const pw = formData.get('pw') as string
+  const nickname = formData.get('nickname') as string
+
+  if (pw.length < 8) {
+    return { result: '', error: '패스워드는 8자 이상이어야 합니다.' }
+  }
+
+  try {
+    await jwtAxios.put('http://localhost:8080/api/member/modify', {
+      email,
+      pw,
+      nickname,
+    })
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    return {
+      result: '',
+      error: err.response?.data?.message || '수정 중 오류가 발생했습니다.',
+    }
+  }
+
+  return { result: 'Modified', error: '' }
+}
 
 function ModifyComponent() {
-  const { loginState } = useCustomLogin()
+  const { loginState, moveToLogin } = useCustomLogin()
+  const [state, action, isPending] = useActionState(modifyAction, initState)
+
+  const closeModal = () => {
+    moveToLogin()
+  }
 
   return (
     <div className="mt-6">
-      <form>
+      {isPending && <PendingModal />}
+
+      {state.error && <div className="bg-amber-500"> {state.error} </div>}
+
+      {state.result && (
+        <ResultModal
+          title="회원 정보 수정 결과 "
+          content={'회원 정보가 수정되었습니다.'}
+          callbackFn={closeModal}
+        />
+      )}
+
+      <form action={action}>
         <div className="flex justify-center">
           <div className="relative mb-4 flex w-full flex-wrap items-stretch">
             <div className="w-1/5 p-6 text-right font-bold">Email</div>
