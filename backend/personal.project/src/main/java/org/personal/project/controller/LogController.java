ecdb30a1944a0b4ce218a64cab922dfdb136facf
peeljunKim <kimkr7410@naver.com>
2025-11-25 22:25:13 +0900

    메트릭 등록 및 그라파나 연결

--- backend/personal.project/src/main/java/org/personal/project/controller/LogController.java
+++ backend/personal.project/src/main/java/org/personal/project/controller/LogController.java
@@ -1,5 +1,7 @@
 package org.personal.project.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class LogController {
 
+    private final MeterRegistry meterRegistry;
+
     @GetMapping("/logTest")
     public void logTest() {
         log.info("info");
@@ -19,5 +23,12 @@ public class LogController {
         log.trace("trace");
         log.warn("warn");
         log.debug("debug");
+
+        Counter.builder("log.test")
+                .tag("class", this.getClass().getName())
+                .tag("method", "log")
+                .description("log")
+                .register(meterRegistry)
+                .increment(); // 카운터 값 증가
     }
 }
