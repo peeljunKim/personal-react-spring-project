1110040537555dee09073e95a3448b0d972a26b4
peeljunKim <kimkr7410@naver.com>
2025-11-26 23:16:41 +0900

    @Timed, @Counted 사용 예시

--- backend/personal.project/src/main/java/org/personal/project/controller/LogController.java
+++ backend/personal.project/src/main/java/org/personal/project/controller/LogController.java
@@ -1,21 +1,20 @@
 package org.personal.project.controller;
 
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
-import lombok.RequiredArgsConstructor;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Gauge;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 @Log4j2
 @RequestMapping("/api")
+@Timed("log.test.timer") // timed도 내부적으로 count가 존재하기 때문에 value 값이 동일하면 count 값이 2가 증가함
 public class LogController {
 
-    private final MeterRegistry meterRegistry;
-
+    @Counted("log.test.count")
     @GetMapping("/logTest")
     public void logTest() {
         log.info("info");
@@ -24,12 +23,12 @@ public class LogController {
         log.warn("warn");
         log.debug("debug");
 
-        Counter.builder("log.test")
-                .tag("class", this.getClass().getName())
-                .tag("method", "log")
-                .description("log")
-                .register(meterRegistry)
-                .increment(); // 카운터 값 증가
+//        Counter.builder("log.test")
+//                .tag("class", this.getClass().getName())
+//                .tag("method", "log")
+//                .description("log")
+//                .register(meterRegistry)
+//                .increment(); // 카운터 값 증가
 
         // xxx/actuator/prometheus 해당 주소에서 메트릭 확인 가능
         // 그라파나에서 log_test_total{method="log"} 이런 식으로 사용하면 됨
