0367c1d5f80608aee8ba67bc5acedf193b9fdb31
peeljunKim <kimkr7410@naver.com>
2026-06-10 17:14:00 +0900

    [chore] BufferingApplicationStartup 관련 추석 추가
    개인적으로 편하려고 추가

--- backend/personal.project/src/main/java/org/personal/project/Application.java
+++ backend/personal.project/src/main/java/org/personal/project/Application.java
@@ -2,14 +2,22 @@ package org.personal.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+//	public static void main(String[] args) {
+//		SpringApplication application = new SpringApplication(Application.class);
+//		application.setApplicationStartup(new BufferingApplicationStartup(2048));
+//		application.run(args);
+//	}
 
 }
