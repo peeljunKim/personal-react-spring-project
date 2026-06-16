4ecfba09fa4639960cee0696a2542aaa80230def
peeljunKim <kimkr7410@naver.com>
2026-06-16 20:56:17 +0900

    [chore] 주석 수정

--- backend/personal.project/src/main/java/org/personal/project/entity/coupon/CouponPolicyStatus.java
+++ backend/personal.project/src/main/java/org/personal/project/entity/coupon/CouponPolicyStatus.java
@@ -4,9 +4,9 @@ package org.personal.project.entity.coupon;
  * 쿠폰 정책 상태
  */
 public enum CouponPolicyStatus {
-    DRAFT,        // 정책 작성 중
+    DRAFT,        // 쿠폰 정책 생성 후 대기
     ACTIVE,       // 발급/사용 가능
     PAUSED,       // 정책 일시 중지
-    ISSUE_CLOSED, // 신규 발급 종료
+    ISSUE_CLOSED, // 이벤트 발급 종료 (사용 가능 단. 기간 등 여러 정책에 따라 사용 종료 될 수 있음)
     CANCELED      // 정책 회수
 }
